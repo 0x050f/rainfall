f181281d5e2341f8727191c4eee37e07bf81f120
Yellow <47715948+y3ll0w42@users.noreply.github.com>
2022-02-01 19:59:12 +0100

    Update source.c

--- bonus3/source.c
+++ bonus3/source.c
@@ -11,13 +11,13 @@ int main(int ac, char *av)
 		
 	fread(buffer,  1, 66, fd);
 	buffer[65] = '\0';
-	buffer[atoi(av[2])] = '\0';
+	buffer[atoi(av[1])] = '\0';
 
 	fread(&buffer[66], 1, 65, fd);
 
 	fclose(fd);
 
-	if (strcmp(buffer, av[2]) == 0)
+	if (strcmp(buffer, av[1]) == 0)
 	{
 		execl("/bin/sh", "sh", 0);
 		return 0;
