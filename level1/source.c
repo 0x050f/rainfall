7d4cbe50cd6af033425e4e66a9ff3e7012cd9c1c
Yellow <47715948+y3ll0w42@users.noreply.github.com>
2022-02-01 18:29:31 +0100

    Update source.c

--- level1/source.c
+++ level1/source.c
@@ -1,9 +1,11 @@
-void run() {
+void run()
+{
 	fwrite("Good... Wait what?\n", 1, 19, 1); 
 	system("/bin/sh");
 }
-int main(int ac, char **av) {
-//
+
+int main(int ac, char **av)
+{
 	char s[0x40];
 	gets(s);
 }
